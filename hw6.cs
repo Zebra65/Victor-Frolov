581ef595a1fbf2ee59d6aa1b4520b536b29ea8a7
Victor Frolov <vgf65@yandex.ru>
2023-07-26 22:46:46 +0300

    HW6

---
+++ hw6.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Задача №8  Определение всех четных чисел в введенном диапазоне ");
+Console.WriteLine("Введите целое число 1: ");
+string userInput = Console.ReadLine () ?? "";
+int number1 = int.Parse (userInput);
+Console.WriteLine("Введите целое число 2: ");
+userInput = Console.ReadLine () ?? "";
+int number2 = int.Parse (userInput);
+Console.WriteLine("Введите целое число 3: ");
+userInput = Console.ReadLine () ?? "";
+int number3 = int.Parse (userInput);
+int max=number1;
+if (number2>max)
+{max=number2;}
+if (number3>max)
+   { max=number3;}
+Console.WriteLine($"Максимальное число {max} ");
