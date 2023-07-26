0b74ed5c54e89b6a5cfc38bba8bc9ac40c9d8940
Victor Frolov <vgf65@yandex.ru>
2023-07-26 22:47:25 +0300

    HW8

---
+++ hw8.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Задача №8");
+Console.WriteLine("Введите целое число 1: ");
+string userInput = Console.ReadLine () ?? "";
+int number1 = int.Parse (userInput);
+Console.WriteLine("Введите целое число 2: ");
+userInput = Console.ReadLine () ?? "";
+int number2 = int.Parse (userInput);
+for (int i = number1; i <= number2; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.ReadKey();
+    //}
