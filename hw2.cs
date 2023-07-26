8e8397be5a5ebe740559b031df9362713709fa87
victor.frolov <vgf65@yandex.ru>
2023-07-26 21:47:31 +0300

    first commit

---
+++ hw2.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Задача №2 Определить большее число");
+Console.WriteLine("Введите целое число 1: ");
+string userInput = Console.ReadLine () ?? "";
+int number1 = int.Parse (userInput);
+Console.WriteLine("Введите целое число 2: ");
+userInput = Console.ReadLine () ?? "";
+int number2 = int.Parse (userInput);
+if (number1==number2)
+{Console.WriteLine($"Число {number1} равно числу Числу {number2} ");}
+else
+if (number1>number2)
+{Console.WriteLine($"Число {number1} больше числа Числfа{number2} ");}
+  else
+{Console.WriteLine($"Число {number2} больше числа Числа {number1} ");}
